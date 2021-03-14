e0feb76c4efbd809a135f9d9fcb832c4943e7890
DevSamuelV <realalloutmusic@gmail.com>
2021-03-14 19:06:52 -0400

    express-boilerplate init

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import helmet from "helmet";
+
+import { json } from "body-parser";
+import { Commands } from "./commands/commands";
+
+const app = express();
+const PORT = 6232;
+
+app.use(json());
+app.use(helmet());
+
+new Commands(app);
+
+app.listen(PORT, () => console.log("Server is Running"));
